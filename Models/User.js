3f478d8c163d48d80b34cbb9fbe420757a3e73c8
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-12-09 16:57:33 -0600

    added mongosse validation

--- Models/User.js
+++ Models/User.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter a email address'],
     unique: true,
     lowercase: true,
+    // Using 3rd party library to validate email address
+    validate: [isEmail, 'Please enter a valid email address'],
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6,
+    required: [true, 'Please enter a valid password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
   },
 });
 
