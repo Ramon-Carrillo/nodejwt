3c426f446767d2d9cca6675512e5c1818fd9e13f
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-12-09 21:34:26 -0600

    Added jwt to the sign up page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../Models/User');
+const jwt = require('jsonwebtoken');
 
 //Handle errors
 const handleErrors = (err) => {
@@ -7,7 +8,7 @@ const handleErrors = (err) => {
 
   // duplicate error code
   if (err.code === 11000) {
-    errors.email = 'that email is already registered';
+    errors.email = 'That email is already registered';
     return errors;
   }
 
@@ -19,6 +20,13 @@ const handleErrors = (err) => {
   }
 
   return errors;
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'net ninja secret', {
+    expiresIn: maxAge,
+  });
 };
 
 module.exports.signup_get = (req, res) => {
@@ -34,8 +42,10 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
     // Status code successfully created
-    res.status(201).json(user);
+    res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
