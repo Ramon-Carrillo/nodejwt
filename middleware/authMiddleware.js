91e2dabf4563b846d5eeda50af436e422e5bd532
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-12-10 18:39:45 -0600

    added conditional rendering

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const User = require('../Models/User');
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
 
   // check if there is a token and verified
   if (token) {
-    jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
+    jwt.verify(token, 'net ninja secret', (err) => {
       if (err) {
         res.redirect('/login');
       } else {
@@ -17,4 +18,26 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
